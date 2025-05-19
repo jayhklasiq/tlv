77fbf8cd1a12f535d2663750587a6c0f936c0767
jayhklasiq <klasik@byui.edu>
2025-05-19 17:18:00 +0000

    refactor: update pricing amount for Leadership Voice Masterclass - Module 1 (PC)

--- src/config/payment.js
+++ src/config/payment.js
@@ -6,7 +6,8 @@ const { registerSuccessMessage, sendPaymentNotification } = require('../services
 const PRICE_CONFIG = {
   1: {
     PC: {
-      amount: 50000, // $150 in cents for Stripe
+      // amount: 50000, // $150 in cents for Stripe
+      amount: 100
       name: 'Leadership Voice Masterclass - Module 1 (PC)',
       maxParticipants: 10
     },
