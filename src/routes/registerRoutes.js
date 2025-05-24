bb4f946d9c91fba4276e2d556d1641229515fb89
jayhklasiq <klasik@byui.edu>
2025-05-24 00:42:42 +0000

    refactor: comment out routes in contact and registration files for future implementation

--- src/routes/registerRoutes.js
+++ src/routes/registerRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const RegisterController = require('../controllers/RegisterController');
 const User = require('../models/User');
 
-// GET route for displaying the registration form
-router.get('/', RegisterController.showForm);
+// // GET route for displaying the registration form
+// router.get('/', RegisterController.showForm);
 
-// POST route for handling form submission
-router.post('/', RegisterController.submit);
+// // POST route for handling form submission
+// router.post('/', RegisterController.submit);
 
 module.exports = router;
