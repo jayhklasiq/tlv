0a3e9d5372e54e5ba8b7d6818f114e1ce1fc0c0f
jayhklasiq <klasik@byui.edu>
2025-05-28 07:51:30 +0000

    feat: add new utility classes for visibility, display, and positioning; update header and home view for improved layout

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ContactController = require('../controllers/ContactController');
 
-// router.get('/', ContactController.index);
+router.get('/', ContactController.index);
 // router.post('/submit', ContactController.submit);
 
 module.exports = router;
